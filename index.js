a0ab20c1e0f4836ba9293b9362303a5571065ccf
Juanpablo Sánchez G <juanpablosg@Juanpablos-iMac-Pro.local>
2021-01-10 23:57:40 -0600

    Name in rainbow colors

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const moment = require('moment')
+const colors = require('colors');
+ 
+
+console.log('Juan Pablo Sánchez González'.rainbow); // rainbow
+ 
+
+console.log("Hola Koders", moment(new Date(), 'yyyy-mm-dd hh:mm').toString())
